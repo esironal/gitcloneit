6ebeb39f2bdf21fea9678544b10ba76e6c1458a6
Lucus <lucusp@users.noreply.github.com>
2015-05-25 23:31:37 -0500

    cloned with gitcloneit

---
+++ assets/js/main.js
@@ -0,0 +1,109 @@
+//8b5f2cac1e672c6bffffdae63efb52e8dec403f9
+//https://dl.dropboxusercontent.com/s/e67p5dgq7a64szd/main.js
+$(function(){
+
+	//$('#tmpdump').text('stuff');
+
+	var GithubUsername, GithubToken;
+
+	//user auth -- oauth only with personal token
+	//$('#cloneit').on('click', function(){
+	
+		$('#authUser').submit(function(e){
+		e.preventDefault();
+		
+		GithubUsername = $('#gitusername').val();
+		GithubToken = $('#gitpassword').val();
+		GithubDir = $('#gitdir').val();
+		//tmp
+		GithubRepo(GithubDir);
+		
+		return false;
+	
+		});	
+	
+	
+	//});
+	
+	
+	var GithubLogin = function(PersonalAccessToken){
+	
+		var github = new Github({
+		
+			token: PersonalAccessToken,
+			auth: 'oauth'
+			
+		
+		});
+		
+		return github;
+	
+	}
+	
+	var GithubRepo = function(repofilepath){
+	
+		var path = repofilepath.split("/");
+		var _repo = path[0];
+		//alert(_repo);	
+		//alert(_path);
+		
+		var _github = GithubLogin(GithubToken);
+		var repo = _github.getRepo(GithubUsername, _repo);
+		
+		/*
+		repo.write('master', 'githubjs.js', function(err, data){
+		
+			$('#tmpdump').text(err);
+			$('#tmpdump').text(data);
+		
+		});
+		*/
+		
+		//tell githubjs the path to write to
+		var dirlen = path.length;
+		var _path = '';
+		var githubjs = $('#cloneurl').val();
+		
+		for(var i=1;i < dirlen;i++){
+		
+			if(i === (dirlen - 1)){
+			
+				_path += path[i];
+			
+			} else {
+			
+				_path += path[i] + "/";	
+				
+			}
+		
+		}		
+		
+		//go get info and clone it
+		$.ajax({
+		
+			url: githubjs
+		
+		}).success(function(data){
+		
+			var content = data;
+			
+			repo.write('master', _path, content, 'cloned with gitcloneit', function(err) {
+    		
+				$('#tmpdump').text(err);
+				$('#tmpdump').text(content);
+  		
+			});		
+		
+		
+		
+		}).error(function(){
+		
+			$('#tmpdump').text('something went wrong.');
+		
+		});
+	
+	}
+
+
+
+});
